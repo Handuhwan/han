27f187b9d940971d19f663efec4321250e4af0d0
Cordverse <jaeho2783@naver.com>
2022-02-08 19:01:40 +0900

    마이페이지컨트롤러 생성

---
+++ Universe/src/main/java/com/universe/controller/MypageController.java
@@ -0,0 +1,15 @@
+package com.universe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/mypage/*")
+public class MypageController {
+	
+	
+
+}
