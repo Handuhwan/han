b64cae548c5cd3906efd3f9fd080eb9d158e011b
Cordverse <jaeho2783@naver.com>
2022-02-09 20:11:47 +0900

    이재호 마이페이지 / 컨트롤러 8:12

--- Universe/src/main/java/com/universe/controller/MypageController.java
+++ Universe/src/main/java/com/universe/controller/MypageController.java
@@ -1,15 +1,27 @@
 package com.universe.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.universe.service.MypageService;
+
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 
 @Controller
-@AllArgsConstructor
 @RequestMapping("/mypage/*")
+@AllArgsConstructor
 public class MypageController {
 	
+	@Setter (onMethod_ = @Autowired)
+	private MypageService service;
+	
+	@GetMapping("/mypage")
+	public void mypage() {
+		
+	}
 	
 
 }
--- Universe/src/main/java/com/universe/service/MypageService.java
+++ Universe/src/main/java/com/universe/service/MypageService.java
@@ -1,8 +1,5 @@
 package com.universe.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface MypageService {
 
 }
